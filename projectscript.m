36aa568049f3e0c11d0f86b60df7da99221aa623
NicholasBermuda <snicholasbarton@gmail.com>
2018-01-30 12:14:13 +0000

    add random i.c. and plot color diagram

--- projectscript.m
+++ projectscript.m
@@ -1,6 +1,6 @@
 % build the network
-N = 5; % number of nodes in the graph
-P = 2; % adjacency bandwidth
+N = 100; % number of nodes in the graph
+P = 28; % adjacency bandwidth
 % topology of the network
 if N == 1
     A = 1;
@@ -15,21 +15,57 @@ alpha = 1; % predation rate
 B = 0.16; % half-saturation constant
 beta = 0.5; % prey efficiency
 m = 0.2; % mortality of prey
-sigma = 3; % coupling strength
+sigma = 1.7; % coupling strength
 
 params = [r K alpha B beta m sigma P]; % vectorise the parameters
 
 % initial conditions
-x0 = rand(2*N,1)*0.5; % random initial conditions
+%x0 = rand(2*N,1)*0.5; % random initial conditions
+
+% initial conditions for N = 100
+x0 = ones(200,1)*0.252; % base condition
+x0(37:46,1) = 0.1;
+x0(37:46,1) = x0(37:46,1) + 0.25*rand(10,1);
+x0(47:66,1) = 0.05;
+x0(47:66,1) = x0(47:66,1) + 0.25*rand(20,1);
+x0(67:100) = 0.033;
+x0(101:136) = 0.102;
+x0(137:146) = 0.05;
+x0(137:146) = x0(137:146) + 0.25*rand(10,1);
+x0(147:166) = 0.05;
+x0(147:166) = x0(147:166) + 0.25*rand(20,1);
+x0(167:200) = 0.09;
 
 % solve the ode
-[T, X] = ode45(@(t, x) RMoscillator(x, params, A), [0 6000], x0);
+[T, X] = ode45(@(t, x) RMoscillator(x, params, A), 0:0.1:6000, x0);
 
 % plot the results
-mask = find(T > 5000 & T < 5150);
+mask = find(T > 5000);% & T < 5500);
 
-% limit cycle
-figure(1)
-V=sum(X(:,1:N),2);
-H=sum(X(:,N+1:2*N),2);
-plot(V(mask),H(mask), 'LineWidth', 1.5) % phase plane
+% % limit cycle
+% figure(1)
+% V=sum(X(:,1:N),2);
+% H=sum(X(:,N+1:2*N),2);
+% plot(V(mask),H(mask), 'LineWidth', 1.5) % phase plane
+% 
+% % H, V vs t
+% figure(2)
+% plot(T(mask),V(mask), 'LineWidth', 1.5)
+% xlabel('$$t$$','Interpreter','latex')
+% ylabel('$$V$$','Interpreter','latex')
+% title('Time series of $$V$$ - Dutta/Banerjee Fig 1bi','Interpreter','latex')
+% 
+% figure(3)
+% plot(T(mask),H(mask), 'LineWidth', 1.5)
+% xlabel('$$t$$','Interpreter','latex')
+% ylabel('$$H$$','Interpreter','latex')
+% title('Time series of $$H$$ - Dutta/Banerjee Fig 1bii','Interpreter','latex')
+
+% %network plot
+% figure(4)
+% networkPlot(A, X(1:5000,:)) %This plots the population data as a network graph
+
+% colour plots
+figure(5)
+Y = X(mask,N+1:2*N);
+imagesc(Y)
