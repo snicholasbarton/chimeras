e52128ffe5fff05b616b7bd8572401f3f9b09a7c
Oliver Bamford <bamford@maths.ox.ac.uk>
2018-02-02 13:53:13 +0000

    Fix 'A' toeplitz matrix to give proper ring structure

--- projectscript.m
+++ projectscript.m
@@ -1,12 +1,12 @@
 % build the network
 
-N = 100; % number of nodes in the graph
-P = 28; % adjacency bandwidth
+N = 20; % number of nodes in the graph
+P = 6; % adjacency bandwidth
 % topology of the network
 if N == 1
     A = 1;
 else
-    A = toeplitz([0,ones(1,P),zeros(1,N-P-2),1]); % adjacency matrix
+    A = toeplitz([0,ones(1,P),zeros(1,N-2*P-1),ones(1,P)]); % adjacency matrix
 end
 
 % parameter values
@@ -21,24 +21,24 @@ sigma = 1.7; % coupling strength
 params = [r K alpha B beta m sigma P]; % vectorise the parameters
 
 % initial conditions
-%x0 = rand(2*N,1)*0.5; % random initial conditions
+x0 = rand(2*N,1)*0.5; % random initial conditions
 
 % initial conditions for N = 100
-x0 = ones(200,1)*0.252; % base condition
-x0(37:46,1) = 0.1;
-x0(37:46,1) = x0(37:46,1) + 0.25*rand(10,1);
-x0(47:66,1) = 0.05;
-x0(47:66,1) = x0(47:66,1) + 0.25*rand(20,1);
-x0(67:100) = 0.033;
-x0(101:136) = 0.102;
-x0(137:146) = 0.05;
-x0(137:146) = x0(137:146) + 0.25*rand(10,1);
-x0(147:166) = 0.05;
-x0(147:166) = x0(147:166) + 0.25*rand(20,1);
-x0(167:200) = 0.09;
+% x0 = ones(200,1)*0.252; % base condition
+% x0(37:46,1) = 0.1;
+% x0(37:46,1) = x0(37:46,1) + 0.25*rand(10,1);
+% x0(47:66,1) = 0.05;
+% x0(47:66,1) = x0(47:66,1) + 0.25*rand(20,1);
+% x0(67:100) = 0.033;
+% x0(101:136) = 0.102;
+% x0(137:146) = 0.05;
+% x0(137:146) = x0(137:146) + 0.25*rand(10,1);
+% x0(147:166) = 0.05;
+% x0(147:166) = x0(147:166) + 0.25*rand(20,1);
+% x0(167:200) = 0.09;
 
 % solve the ode
-[T, X] = ode45(@(t, x) RMoscillator(x, params, A), 0:0.1:6000, x0);
+[T, X] = ode45(@(t, x) RMoscillator(x, params, A), 0:6000, x0);
 
 % plot the results
 mask = find(T > 5000 & T < 5500);
@@ -63,8 +63,8 @@ mask = find(T > 5000 & T < 5500);
 % title('Time series of $$H$$ - Dutta/Banerjee Fig 1bii','Interpreter','latex')
 
 % %network plot
-% figure(4)
-% networkPlot(A, X(1:5000,:)) %This plots the population data as a network graph
+figure(4)
+networkPlot(A, X) %This plots the population data as a network graph
 
 % colour plots - top is vegetation, bottom is herbivores
 figure(5)
