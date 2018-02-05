e49ae5a9ed07325a467250c2cc95753cb6a753ab
Angelica Grusovin <angelicagrusovin@Angelicas-MacBook-Pro.local>
2018-02-05 17:55:31 +0000

    cleaned Chimera function

--- Chimera.m
+++ Chimera.m
@@ -1,19 +1,21 @@
 function state = Chimera(P,sigma)
+% function that classifies the state of system of 100 nodes with ring topology for
+% different sigmas and coupling coefficients
 
-% build the network
-%p=[1,3,5,10,15,30];
-%for i=1:5
+% state 1: CD       - all nodes constant / no oscillation
+% state 2: CSOD     - 
+% state 3: Sync     - all nodes are oscillating
+% state 4: AC+Death -
+
 N = 100; % number of nodes in the graph
-%P = p(i) % adjacency bandwidth
-% topology of the network
 
-if N == 1
-    A = 1;
-else
-    A = toeplitz([0,ones(1,P),zeros(1,N-2*P-1),ones(1,P)]); % adjacency matrix
-end
 
-% parameter values
+%%%%%%%%%%%% topology of the network %%%%%%%%%%
+
+A=makeAdjMat(N,'ring',P);
+
+%%%%%%%%%%%% parameter values %%%%%%%%%%%%%%%%%
+
 r = 0.5; % growth rate of prey
 K = 0.5; % carrying capacity of prey
 alpha = 1; % predation rate
@@ -23,101 +25,76 @@ m = 0.2; % mortality of prey
 
 params = [r K alpha B beta m sigma P]; % vectorise the parameters
 
-% initial conditions
-%x0 = rand(2*N,1)*0.5; % random initial conditions
+%%%%%%%%%%%% initial conditions %%%%%%%%%%
 
-%initial conditions for N = 100
-load('x0')
-% x0 = ones(200,1)*0.252; % base condition
-% x0(37:46,1) = 0.1;
-% x0(37:46,1) = x0(37:46,1) + 0.25*rand(10,1);
-% x0(47:66,1) = 0.05;
-% x0(47:66,1) = x0(47:66,1) + 0.25*rand(20,1);
-% x0(67:100) = 0.033;
-% x0(101:136) = 0.102;
-% x0(137:146) = 0.05;
-% x0(137:146) = x0(137:146) + 0.25*rand(10,1);
-% x0(147:166) = 0.05;
-% x0(147:166) = x0(147:166) + 0.25*rand(20,1);
-% x0(167:200) = 0.09;
+% x0 = rand(2*N,1)*0.5; % random initial conditions
+
+% load('x0') %fixed random initial conditions
+
+x0 = ones(200,1)*0.252; % base condition
+x0(37:46,1) = 0.1;
+x0(37:46,1) = x0(37:46,1) + 0.25*rand(10,1);
+x0(47:66,1) = 0.05;
+x0(47:66,1) = x0(47:66,1) + 0.25*rand(20,1);
+x0(67:100) = 0.033;
+x0(101:136) = 0.102;
+x0(137:146) = 0.05;
+x0(137:146) = x0(137:146) + 0.25*rand(10,1);
+x0(147:166) = 0.05;
+x0(147:166) = x0(147:166) + 0.25*rand(20,1);
+x0(167:200) = 0.09;
+
+%%%%%%%%%%%% solve ode %%%%%%%%%%
 
 % solve the ode
 [T, X] = ode45(@(t, x) RMoscillator(x, params, A), 0:6000, x0);
 
-% plot the results
-mask = find(T > 5000 & T <= 6000);
+%%%%%%%%%%%% organise data %%%%%%%%%%
 
-% % limit cycle
-% figure(1)
-% V=sum(X(:,1:N),2);
-% H=sum(X(:,N+1:2*N),2);
-% plot(V(mask),H(mask), 'LineWidth', 1.5) % phase plane
-%
-% % H, V vs t
-% figure(2)
-% plot(T(mask),V(mask), 'LineWidth', 1.5)
-% xlabel('$$t$$','Interpreter','latex')
-% ylabel('$$V$$','Interpreter','latex')
-% title('Time series of $$V$$ - Dutta/Banerjee Fig 1bi','Interpreter','latex')
-%
-% figure(3)
-% plot(T(mask),H(mask), 'LineWidth', 1.5)
-% xlabel('$$t$$','Interpreter','latex')
-% ylabel('$$H$$','Interpreter','latex')
-% title('Time series of $$H$$ - Dutta/Banerjee Fig 1bii','Interpreter','latex')
+mask = T > 5000 & T <= 6000;
 
-% %network plot
+V=X(:,1:N);
+H=X(:,N+1:2*N);
 
-%figure(4)
-%networkPlot(A, X) %This plots the population data as a network graph
+dev=std(V(mask,:)); 
 
-% %colour plots - top is vegetation, bottom is herbivores
-% figure(5)
-% colorbar
-% subplot(2,1,1)
-Y = X(mask,1:N);
-% imagesc(Y)
-% xlabel('$$i$$','Interpreter','latex')
-% ylabel('$$t$$','Interpreter','latex')
-% title('$$V$$ spatiotemporal colour map','Interpreter','latex')
-% subplot(2,1,2)
-% Z = X(mask,N+1:2*N);
-% imagesc(Z)
-% xlabel('$$i$$','Interpreter','latex')
-% ylabel('$$t$$','Interpreter','latex')
-% title('$$H$$ spatiotemporal colour map','Interpreter','latex')
+%%%%%%%%%%%% classification %%%%%%%%%%
 
-dev=std(Y(:,1:N)); % Y veg Z herbivore
-flag1=0;
-flag2=0;
+cond1=0;
+cond2=0;
 
 if any(dev<0.01)
-    flag1=1;
+    cond1=1;
 end
 if any(dev>0.01)
-    flag2=1;
+    cond2=1;
 end
 
 
-if flag1==1 && flag2 ==0
-    state=1; % CD all zero SD
+if cond2 == 0 % if all sd are 0
+    state=1; 
+    disp([P sigma]);
     disp('CD');
 end
 
-if flag1==1 && flag2 ==1
-    zeromask=find(dev>sqrt(eps));
-    ACsearch=range(dev(zeromask));
-    if ACsearch>0.1*max(dev)
-        state=4;
-        disp('AC + Death');
+if cond1==1 && cond2 ==1
+
+    sdrange=range(dev(dev>sqrt(eps)));
+    
+    if sdrange>0.1*max(dev)
+        state=4; 
+        disp([P sigma]);
+        disp('AC+Death');   
     else
-        state=2; % CSOD mixed zero sd and nonzero sd
+        state=2; 
+        disp([P sigma]);
         disp('CSOD');
     end
 end
 
-if flag1==0 && flag2 ==1
-    state=3; % Sync oscillation all nonzero sd
+if cond1 == 0 % if all sd are non-0 
+    state=3; 
+    disp([P sigma]);
     disp('Sync');
 end
 
