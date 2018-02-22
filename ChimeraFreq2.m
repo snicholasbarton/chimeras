c8175ac5b0aff343a651c2da58a756de1525a9c7
Angelica Grusovin <angelicagrusovin@Angelicas-MacBook-Pro.local>
2018-02-22 17:14:43 +0000

    debugged freq chimera

--- ChimeraFreq2.m
+++ ChimeraFreq2.m
@@ -60,6 +60,8 @@ state=0; % no state
 
 % assuming it is not CD
 freqvec2=freqvec(any(freqvec,2)==1,:);
+I = 1:N;
+I=I(any(freqvec,2)==1);
 
 if max(std(freqvec2))<10^(-4) % if all nodes have the same frequencies
     state=1; % sync
@@ -69,9 +71,13 @@ else
     multiplevec = find(n > 1);
     for i =1:length(multiplevec)
         multiple=multiplevec(i);
-        index = find(ismember(bin, multiple));
+        index2 = I(ismember(bin, multiple));
         
-        if any(A(index, index))==1
+        logical=A(index2,index2);
+        
+        logical=logical(:);
+        
+        if any(logical)==1
             state=2; % frequency chimera
             break
         end
