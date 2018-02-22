a7717bfb7f9459b402dcd3025f6b837daf3bcf04
NicholasBermuda <snicholasbarton@gmail.com>
2018-02-22 14:13:16 +0000

    updated classify, flags, find states

---
+++ find_death.m
@@ -0,0 +1,20 @@
+function D = find_death(X)
+%find_SS Checks if any of the nodes are dead steady states
+% INPUTS:
+%   X: matrix of (time x nodes) for either V or H
+% OUTPUTS:
+%   D: the indices of dead nodes in X
+
+% tolerance for `death'
+tol=1e-7;
+
+% X is of dimension (time x nodes)
+N = size(X,2);
+
+% transients discarded previously
+
+% look for Death and record where you find it
+D=1:N;
+D=D(mean(abs(X)) < tol);
+
+end
