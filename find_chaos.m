d57b994e5b2bd2992551b0d9783cf114d39acd79
Angelica Grusovin <angelicagrusovin@Angelicas-MacBook-Pro.local>
2018-03-07 18:14:03 +0000

    changed 0-1 test tolerance

--- find_chaos.m
+++ find_chaos.m
@@ -13,7 +13,7 @@ Z = []; % initialise Z
 
 % look for chaos and record where you find it
 for n = 1:N
-    chaos_Xn = z1test(X(1:10:end,n))
+    chaos_Xn = z1test(X(1:10:end,n));
     if abs(1 - chaos_Xn) < 0.75
         Z = [Z;n];
     end
